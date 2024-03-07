422f08c9c6175bf83e648142338bf6d0c7291429
will yin <826895143@qq.com>
2024-03-07 17:26:28 +0800

    feat: reset test file

--- test/wprom_test.go
+++ test/wprom_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/will-yinchengxin/wprom_c/core"
+	wprom "github.com/will-yinchengxin/wprom_c/core"
 	"strconv"
 	"testing"
 )
@@ -11,13 +11,13 @@ import (
 func TestWpromC(t *testing.T) {
 	gin.SetMode("release")
 	r := gin.New()
-	reqMetric := core.SetGaugeVecMetric(
+	reqMetric := wprom.SetGaugeVecMetric(
 		"will_test",
 		"requests_total",
 		"How many HTTP requests processed, partitioned by status code and HTTP method.",
-		[]string{"code", "method", "handler", "host"},
+		[]string{"code", "method", "handler", "host", "path"},
 	)
-	p := core.NewPrometheus(reqMetric).
+	p := wprom.NewPrometheus(reqMetric).
 		SetSubsystemAndRegisterMetrics("will")
 	p.Use(r, HandleFun(p))
 	r.GET("/", func(c *gin.Context) {
@@ -27,13 +27,15 @@ func TestWpromC(t *testing.T) {
 	r.Run(":29090")
 }
 
-func HandleFun(p *core.Prometheus) gin.HandlerFunc {
+func HandleFun(p *wprom.Prometheus) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		for key, _ := range p.MetricsList {
 			c.Next()
 			if key == "will_test" {
 				status := strconv.Itoa(c.Writer.Status())
-				p.MetricsList[key].MetricCollector.(*prometheus.GaugeVec).WithLabelValues(status, c.Request.Method, c.HandlerName(), c.Request.Host).Inc()
+				p.MetricsList[key].MetricCollector.(*prometheus.GaugeVec).
+					WithLabelValues(status, c.Request.Method, c.HandlerName(), c.Request.Host, c.Request.URL.Path).
+					Inc()
 			}
 		}
 	}
